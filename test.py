30bbc1b3365f061abb2a7930426d1cbaaeeff9fb
AzizaSuleyman <azizadubna@gmail.com>
2020-09-09 02:08:08 -0500

    First commit

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 00:36:45 2020
+
+@author: azizasuleymanzade
+"""
+
+import tkinter as tk
+root = tk.Tk()
+label = tk.Label(root, text="Hello World", padx=10, pady=10)
+label.pack()
+root.mainloop()
